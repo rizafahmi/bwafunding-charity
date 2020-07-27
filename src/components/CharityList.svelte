4f3b352ddcdc3d7caaf1458790c41206e7ada380
Riza Fahmi <rizafahmi@gmail.com>
2020-07-27 21:18:11 +0700

    Convert HTML to svelte components

--- src/components/CharityList.svelte
+++ src/components/CharityList.svelte
@@ -1,5 +1,8 @@
 <script>
+  import Modal from "./Modal.svelte";
+
   export let charities;
+  let isModalOpen = false;
 
   function calculateFunded(pledged, target) {
     return Math.round((1 / (target / pledged)) * 100);
@@ -18,11 +21,23 @@
     const oneDay = 24 * 60 * 60 * 1000;
     return Math.round(Math.abs(delta / oneDay));
   }
+
+  function handleButton() {
+    isModalOpen = true;
+  }
+
+  function handleCloseModal() {
+    isModalOpen = false;
+  }
 </script>
 
 <style>
   .xs-list-with-content {
     font-size: 12px;
+  }
+  .show {
+    display: block;
+    background-color: rgba(0, 0, 0, 0.45);
   }
 </style>
 
@@ -47,80 +62,88 @@
       {#each charities as charity}
         <div class="row">
           <div class="col-lg-4 col-md-6">
-            <!-- modal goes here -->
-            <!-- Modal -->
-            <div
-              class="modal fade"
-              id="exampleModal"
-              tabindex="-1"
-              role="dialog"
-              aria-labelledby="exampleModalLabel"
-              aria-hidden="true">
-              <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">
-                      Splash Drone 3 a Fully Waterproof Drone that floats
-                    </h5>
-                    <button
-                      type="button"
-                      class="close"
-                      data-dismiss="modal"
-                      aria-label="Close">
-                      <span aria-hidden="true">&times;</span>
-                    </button>
-                  </div>
-                  <div class="modal-body">
-                    <form>
-                      <div class="form-group">
-                        <label for="exampleInputAmount">Amount donation</label>
-                        <input
-                          required
-                          type="number"
-                          class="form-control"
-                          id="exampleInputAmount"
-                          aria-describedby="amountHelp"
-                          placeholder="Enter amount" />
+            {#if isModalOpen === true}
+              <Modal>
+                <!-- modal goes here -->
+                <!-- Modal -->
+                <div
+                  class="modal fade show"
+                  id="exampleModal"
+                  tabindex="-1"
+                  role="dialog"
+                  aria-labelledby="exampleModalLabel">
+                  <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">
+                          {charity.title}
+                        </h5>
+                        <button
+                          type="button"
+                          class="close"
+                          data-dismiss="modal"
+                          aria-label="Close"
+                          on:click={handleCloseModal}>
+                          <span aria-hidden="true">&times;</span>
+                        </button>
                       </div>
-                      <div class="form-group">
-                        <label for="exampleInputName">Your name</label>
-                        <input
-                          required
-                          type="text"
-                          class="form-control"
-                          id="exampleInputName"
-                          aria-describedby="nameHelp"
-                          placeholder="Enter full name" />
+                      <div class="modal-body">
+                        <form>
+                          <div class="form-group">
+                            <label for="exampleInputAmount">
+                              Amount donation
+                            </label>
+                            <input
+                              required
+                              type="number"
+                              class="form-control"
+                              id="exampleInputAmount"
+                              aria-describedby="amountHelp"
+                              placeholder="Enter amount" />
+                          </div>
+                          <div class="form-group">
+                            <label for="exampleInputName">Your name</label>
+                            <input
+                              required
+                              type="text"
+                              class="form-control"
+                              id="exampleInputName"
+                              aria-describedby="nameHelp"
+                              placeholder="Enter full name" />
+                          </div>
+                          <div class="form-group">
+                            <label for="exampleInputEmail1">
+                              Email address
+                            </label>
+                            <input
+                              required
+                              type="email"
+                              class="form-control"
+                              id="exampleInputEmail1"
+                              aria-describedby="emailHelp"
+                              placeholder="Enter email" />
+                          </div>
+                          <div class="form-check">
+                            <input
+                              type="checkbox"
+                              class="form-check-input"
+                              id="exampleCheck1" />
+                            <label class="form-check-label" for="exampleCheck1">
+                              I Agree
+                            </label>
+                          </div>
+                        </form>
                       </div>
-                      <div class="form-group">
-                        <label for="exampleInputEmail1">Email address</label>
-                        <input
-                          required
-                          type="email"
-                          class="form-control"
-                          id="exampleInputEmail1"
-                          aria-describedby="emailHelp"
-                          placeholder="Enter email" />
+                      <div class="modal-footer">
+                        <button type="button" class="btn btn-primary">
+                          Continue
+                        </button>
                       </div>
-                      <div class="form-check">
-                        <input
-                          type="checkbox"
-                          class="form-check-input"
-                          id="exampleCheck1" />
-                        <label class="form-check-label" for="exampleCheck1">
-                          I Agree
-                        </label>
-                      </div>
-                    </form>
-                  </div>
-                  <div class="modal-footer">
-                    <button type="button" class="btn btn-primary">
-                      Continue
-                    </button>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </div>
+              </Modal>
+            {/if}
             <div class="xs-popular-item xs-box-shadow">
               <div class="xs-item-header">
 
@@ -187,13 +210,13 @@
 
                 <span class="xs-separetor" />
 
-                <a
-                  href="#"
+                <button
+                  on:click={handleButton}
                   data-toggle="modal"
                   data-target="#exampleModal"
                   class="btn btn-primary btn-block">
                   Donate This Cause
-                </a>
+                </button>
               </div>
               <!-- .xs-item-content END -->
             </div>
